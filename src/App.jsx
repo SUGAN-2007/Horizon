53290aaff287b8f4553a56ee9364cd30fcb4f2c7
SUGAN-2007 <developer.sugan@gmail.com>
2025-12-15 12:23:09 +0530

    fea and num and ..

--- src/App.jsx
+++ src/App.jsx
@@ -23,9 +23,6 @@ function App() {
             }
         };
         fetchProducts();
-        // fetch('https://fakestoreapi.com/products')
-        //     .then(res => res.json())
-        //     .then(data => setProducts(data))
     }, []);
     return (
         <Routes>
